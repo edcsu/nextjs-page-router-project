fd53c9fb1ff3f9023bb474397a7fd472dc2b16ed
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-26 16:45:06 +0300

    feat: connect to mongo db

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,28 @@
 // /api/new-meetup
 
-const handler = (req, res) => {
+import { MongoClient } from "mongodb"
+
+const handler = async (req, res) => {
     if (req.method === 'POST') {
         const data = req.body
 
         const { title, image, address, description } = data
+        
+        const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hyowl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+
+        const client = await MongoClient.connect(uri)
+        
+        const db = client.db()
+
+        const meetupsCollection = db.collection('meetups')
+
+        const result = await meetupsCollection.insertOne(data)
+
+        console.log(result)
+
+        client.close()
+
+        res.status(201).json({ message: 'Meetup added', ...result})
     }
 }
 
