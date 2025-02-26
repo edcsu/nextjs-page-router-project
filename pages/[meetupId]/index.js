47282c57ad0ca143d2fbc65e7016d13f05ac4b31
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-26 15:59:20 +0300

    refactor: set static props for meet details

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,16 +1,34 @@
 import MeetUpDetail from '../../components/meetups/MeetupDetail'
 
-const MeetUpDetails = () => {
+const MeetUpDetails = (props) => {
     return (
         <>
             <MeetUpDetail
-                image="https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                title="First meetup"
-                address="Kampala, Uganda"
-                description="Meetup description"
+                image={props.image}
+                title={props.title}
+                address={props.address}
+                description={props.description}
             />
         </>
     )
+}
+
+export async function detStaticProps(context) {
+    // fetch data for a single meetup
+
+    const meetupId = context.params.meetupId
+    
+    return {
+        props: {
+            meetup :{
+                id: meetupId,
+                image : "https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                title : "First meetup",
+                address :"Kampala, Uganda",
+                description : "Meetup description"
+            }
+        }
+    }
 }
 
 export default MeetUpDetails
