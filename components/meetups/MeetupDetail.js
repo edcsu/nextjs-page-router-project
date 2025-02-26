0e1aab390e03ef212a915ab397efd00425c5ddf1
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-26 13:57:51 +0300

    style: add effect on detail page

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,6 +1,8 @@
+import classes from './MeetupDetail.module.css'
+
 const MeetUpDetail = (props) => {
     return (
-        <>
+        <section className={classes.detail}>
             <img
                 src={props.image}
                 alt={props.title} 
@@ -8,7 +10,7 @@ const MeetUpDetail = (props) => {
             <h1>{props.title}</h1>
             <address>{props.address}</address>
             <p>{props.description}</p>
-        </>
+        </ section>
     )
 }
 
