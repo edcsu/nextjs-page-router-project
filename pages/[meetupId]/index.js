2b5bd8ee32ee50396de3d0ae3231c285a395ac93
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-26 13:29:59 +0300

    style: add meetup details content

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,15 @@
+import Image from "next/image"
+
 const MeetUpDetails = () => {
     return (
         <>
+            <img
+                src="https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt="First meetup" 
+            />
+            <h1>First meetup</h1>
+            <address>Kampala, Uganda</address>
+            <p>Meetup description</p>
         </>
     )
 }
